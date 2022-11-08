eca13aeb97e7953a75c9f8df0d1e9330fd2d4926
carecamis22 <111764679+carecamis22@users.noreply.github.com>
2022-11-08 01:48:33 +0000

    Nov 7, 2022, 9:48 PM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-﻿public class EnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public float speed;
     public bool vertical;
@@ -13,11 +13,14 @@ using UnityEngine;
     float timer;
     int direction = 1;
     
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -37,16 +40,20 @@ using UnityEngine;
         
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction;;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction;;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
         
         rigidbody2D.MovePosition(position);
     }
-
+    
     void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController >();
