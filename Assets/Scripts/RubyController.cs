2f2e18be1414a26d001d9f611e310edf05dee3c8
carecamis22 <111764679+carecamis22@users.noreply.github.com>
2022-11-08 02:07:40 +0000

    Nov 7, 2022, 10:07 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubyController : MonoBehaviour
+﻿public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
-
+    
+    public GameObject projectilePrefab;
+    
     public int health { get { return currentHealth; }}
     int currentHealth;
     
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
     
@@ -55,6 +57,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+        
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
     
     void FixedUpdate()
@@ -70,7 +77,6 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
-            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             
@@ -80,6 +86,16 @@ public class RubyController : MonoBehaviour
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
 
